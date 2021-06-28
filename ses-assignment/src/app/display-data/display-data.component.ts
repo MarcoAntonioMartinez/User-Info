c7a47c75e5adddc2e57376a3910e7fcc0a774b35
martz190 <marco.antoni.martinez@gmail.com>
2021-06-28 18:24:52 -0500

    Added editing functionality to display data page

--- ses-assignment/src/app/display-data/display-data.component.ts
+++ ses-assignment/src/app/display-data/display-data.component.ts
@@ -8,7 +8,7 @@ import { User } from '../ses-assignment/user';
   styleUrls: ['./display-data.component.css']
 })
 export class DisplayDataComponent implements OnInit {
-  
+ 
   //form instance used to hold array of users
   public displayForm: {
 	users: User[];
@@ -16,6 +16,17 @@ export class DisplayDataComponent implements OnInit {
 	ageMax: number,
 	ageAvg: number
   };
+  
+  model = " "
+  displayUser: User = new User();
+  enableEdit = false;
+  enableEditIndex = null;
+  editUser = "";
+  editName = "";
+  isEditable: boolean = false;
+  
+  //used for switch statement
+  userNum = 0;
   
   constructor(private userService: UserService) { 
     this.displayForm = {
@@ -32,7 +43,7 @@ export class DisplayDataComponent implements OnInit {
 	
 	//calculate all user's age
 	let userAge: number[] = [];
-	//userAge: [];
+	
 	for(let user of this.displayForm.users) {
 	
 	userAge.push(this.calculateAge(user));
@@ -44,7 +55,41 @@ export class DisplayDataComponent implements OnInit {
 
 	this.displayForm.ageAvg = +(this.getAgeAvg(userAge).toFixed(2));
   }
- 
+  
+	enableEditing(e: any,i: any){
+		this.enableEdit = true;
+		this.enableEditIndex = i;
+		console.log(i,e);
+		console.log("before assignment: " + this.isEditable);
+		this.isEditable = true;
+		console.log("after assignment: " + this.isEditable);
+	}
+	
+	saveEdit(event: any, name: string){
+		this.editUser = event.target.outerText as string;
+		this.editName = name;
+	}
+	
+	submitEdit(index: number){
+	  switch(this.userNum)
+	  {
+		case 1:
+			this.displayForm.users[index].firstName = this.editUser;
+			break;
+		case 2:
+			this.displayForm.users[index].lastName = this.editUser;
+			break;
+		case 3:
+			this.displayForm.users[index].sex = this.editUser;
+			break;
+		case 4:
+			let userDate = new Date(this.editUser);
+			this.displayForm.users[index].birthday = userDate;
+			break;
+	  }	
+	  
+	  console.log(this.displayForm.users);
+	}
 	calculateAge(user: User){
 	let age = 0;
 	var currentTime = new Date();
@@ -54,18 +99,10 @@ export class DisplayDataComponent implements OnInit {
 	//get the user's birth year
 	var year = user.birthday.getFullYear();
 	
-	//year = Date.parse(birthYear);
-	//console.log("this is typeof year:")
-	//console.log(typeof(year))
-	
 	age = currentYear - year;
 	
 	return age;
-	//then do comparison between all users age. might need for loop for that and store in array or something
-	//how do i edit a table entry? is that possible? or do i need to actually do what that one person did with dogs and ng template?
 	
-	//add user would need me to push to displayForm users array
-	//maybe do same thing as that guy with link button thing that adds another input field
 	}
 	
 	getAgeMin(age: number[]){
