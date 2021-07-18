07f6311550765ba23dead035cffadec27e051283
martz190 <marco.antoni.martinez@gmail.com>
2021-07-18 11:36:34 -0500

    Removed unused spec.ts files

--- ses-assignment/src/app/user-sex.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserSexPipe } from './user-sex.pipe';
-
-describe('UserSexPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UserSexPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
