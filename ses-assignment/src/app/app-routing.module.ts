893a1f5e9d65307b4e5cf3b2f3ce134a352890d9
Marco Martinez <marco.antoni.martinez@gmail.com>
2022-02-09 16:57:05 -0600

    Update app-routing.module.ts

--- ses-assignment/src/app/app-routing.module.ts
+++ ses-assignment/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SesAssignmentComponent } from './ses-assignment/ses-assignment.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 import { DisplayDataComponent } from './display-data/display-data.component';
 
 const routes: Routes = [
 	{
 	  path: '',
-	  component: SesAssignmentComponent
+	  component: UserInfoComponent
 	},
 	{
 	  path: 'display-data',
