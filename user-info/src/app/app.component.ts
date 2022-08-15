8196ad0982dbde979849366b188b956b5b872b76
martz190 <marco.antoni.martinez@gmail.com>
2022-08-15 15:05:51 -0500

    Changed title of app

--- user-info/src/app/app.component.ts
+++ user-info/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser'; 
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ses-assignment';
+  title = 'user-info';
   
-  constructor(){}
+  constructor(private titleService: Title){
+	titleService.setTitle("User Information Form");
+  }
 }
